59b651c2b8de24b9a9432fe6e9f7533c78e73647
Bob Schmertz <rschmertz@yahoo.com>
2016-01-04 01:52:01 -0500

    Minimal regex testing working for tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-fn tokenize(s: &str) {
-    println!("in tokenize");
+extern crate regex;
+use regex::Regex;
+
+fn tokenize(s: &str) -> Vec<&str> {
+    let re = Regex::new(r"[ \t]+").unwrap();
+    let tokens: Vec<&str> = re.split(s).collect();
+    return tokens;
 }
 
 #[test]
@@ -7,7 +12,7 @@ fn test_tokenize() {
     println!("in test_tokenize");
     let teststr1 = "key1 value1  key2  value2 ";
     let result1 = tokenize(teststr1);
-    assert!(true);
+    assert_eq!(result1, vec!("key1", "value1", "key2", "value2"));
 }
 
 fn main() {
