9a442f73e089f8cea69c4308e75e67fd5710a135
Bob Schmertz <rschmertz@yahoo.com>
2016-01-04 01:26:27 -0500

    Add skeleton tokenize and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn tokenize(s: &str) {
+    println!("in tokenize");
+}
+
+#[test]
+fn test_tokenize() {
+    println!("in test_tokenize");
+    let teststr1 = "key1 value1  key2  value2 ";
+    let result1 = tokenize(teststr1);
+    assert!(true);
+}
+
 fn main() {
     println!("Hello, world!");
 }
